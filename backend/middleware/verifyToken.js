d22f2e75f100145cc24f8a75f16bc64127ac4444
daVinciManuel <mgmt.manuel02@gmail.com>
2025-10-13 15:13:56 +0200

    feat(jwt): Add middleware

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken"
+
+export function verifyToken(req, res, next){
+    const token = req.headers["authorization"]
+    if(!token) return res.status(403).json({message: "Token requerido."});
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(401).json({Message: "token invalido"});
+        req.userId = decoded.id;
+        next();
+    })
+}
