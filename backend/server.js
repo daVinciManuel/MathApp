e0cb0d3d6a9a4c3de3d46243906b40f30add3b24
PROD\manuel.martinezt <manuel.martinezt@alten.es>
2025-10-27 01:19:02 +0100

    Refactor: from nodemon to .env

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
-import express from 'express';
+import dotenv from "dotenv";
+import express from "express";
 import { sequelize } from "./db/conexion.js";
 import authRoutes from "./routes/auth.js";
 import gameRoutes from "./routes/game.js";
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use("/api/auth", authRoutes)
-app.use("/api/game", gameRoutes)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
+app.use("/api/game", gameRoutes);
 
-// sequelize.sync()
-    // .then(() =>{
-        app.listen(5000,() => {
-            console.log('Server is running on http://localhost:5000')
-        })
-    // })
-    // .catch((err) => console.log("Error creating DB: " + err))
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(5000, () => {
+      console.log("Server is running on http://localhost:5000");
+    });
+  })
+  .catch((err) => console.log("Error creating DB: " + err));
