8a9ab003b569d246a1d878568e7a3f75384a69b7
PROD\manuel.martinezt <manuel.martinezt@alten.es>
2025-10-27 09:30:05 +0100

    Changing DB Credentials

--- backend/db/conexion.js
+++ backend/db/conexion.js
@@ -1,17 +1,29 @@
+import dotenv from "dotenv";
+import fs from "fs";
 import { Sequelize } from "sequelize";
+dotenv.config();
 
-export const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        dialect: "mysql"
-    }
-)
+export const conn = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+        ca: fs.readFileSync("./ca.pem").toString(),
+      },
+    },
+  }
+);
 
-sequelize.authenticate()
-.then(() => console.log("MySQL conneted OK."))
-.catch((err) => console.log("MySQL connection error: " + err))
+conn
+  .authenticate()
+  .then(() => console.log("MySQL conneted OK."))
+  .catch((err) => console.log("MySQL connection error: " + err));
 
 // export default sequelize;
--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,14 +1,17 @@
 import { DataTypes } from "sequelize";
-import { sequelize  } from "../db/conexion.js";
+import { conn } from "../db/conexion.js";
 // const sequelize = require('./con')
 
-const User = sequelize.define("User",{
-    name: {type: DataTypes.STRING(50),allowNull:false,unique:true},
-    fullname: {type: DataTypes.STRING(50),},
-    email: {type: DataTypes.STRING(60),allowNull:false,unique:true},
-    age: {type: DataTypes.INTEGER},
-    pass: {type: DataTypes.STRING, allowNull:false}
-}, { timestamps: false }
-)
+const User = conn.define(
+  "User",
+  {
+    name: { type: DataTypes.STRING(50), allowNull: false, unique: true },
+    fullname: { type: DataTypes.STRING(50) },
+    email: { type: DataTypes.STRING(60), allowNull: false, unique: true },
+    age: { type: DataTypes.INTEGER },
+    pass: { type: DataTypes.STRING, allowNull: false },
+  },
+  { timestamps: false }
+);
 // module.exports = User;
-export default User
+export default User;
