e575acff62ba829415a85343adc989d393ed2e7a
PROD\manuel.martinezt <manuel.martinezt@alten.es>
2025-10-28 13:34:02 +0100

    FIX(DB-users): Users.fullname renamed to: Users.lastname

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -6,7 +6,7 @@ const User = conn.define(
   "User",
   {
     name: { type: DataTypes.STRING(50), allowNull: false, unique: true },
-    fullname: { type: DataTypes.STRING(50) },
+    lastname: { type: DataTypes.STRING(50) },
     email: { type: DataTypes.STRING(60), allowNull: false, unique: true },
     age: { type: DataTypes.INTEGER },
     pass: { type: DataTypes.STRING, allowNull: false },
