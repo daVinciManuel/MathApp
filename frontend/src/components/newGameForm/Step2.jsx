9ea1ff8f74e855067760b058c12df30ca026b203
daVinciManuel <mgmt.manuel02@gmail.com>
2025-11-21 08:51:39 +0100

    feat(newGame): Form updated

--- frontend/src/components/newGameForm/Step2.jsx
+++ frontend/src/components/newGameForm/Step2.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import DataExe from "./DataExe";
 const Step2 = ({ change, currentExe }) => {
   const [numTerms, setNumTerms] = useState(2)
   console.log(numTerms)
@@ -8,7 +9,8 @@ const Step2 = ({ change, currentExe }) => {
     <br />
     <input onChange={(e) => { setNumTerms(e.target.value) }} type="number" name="numTerms" id="numTerms" min='2' max='10' />
 
-    // Load Exercise config
+    {/* // Load Exercise config */}
+    <DataExe numTerms={numTerms} />
     <br />
   </step-2>
   )
